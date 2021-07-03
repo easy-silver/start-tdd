fc87585eb4bc9e88f9e17fe78b8eeb0554ed2ef8
Timo <developer.timo@gmail.com>
2021-07-03 20:09:01 +0900

    모든 조건을 충족하는 테스트 - 테스트를 통과하도록 수정

--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -1,12 +1,20 @@
 package chap02;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static chap02.PasswordStrength.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * 암호 검사기 테스트
+ *
+ * - 길이가 8글자 이상
+ * - 0부터 9 사이의 숫자를 포함
+ * - 대문자 포함
+ *
+ * 1. 세 규칙을 모두 충족하면 강함
+ * 2. 2개의 규칙을 충족하면 보통
+ * 3. 1개 이하의 규칙을 충족하면 약함
  */
 class PasswordStrengthMeterTest {
 
@@ -17,7 +25,10 @@ class PasswordStrengthMeterTest {
     void meetsAllCriteria_Then_Strong() {
         PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab12!@AB");
-        assertEquals(PasswordStrength.STRONG, result);
+        assertEquals(STRONG, result);
+
+        PasswordStrength result2 = meter.meter("abc1!Add");
+        assertEquals(STRONG, result2);
     }
 
 }
