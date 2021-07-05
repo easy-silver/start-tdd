6a9f121c4ccb7cee0e752af8c0900c13dc5e2955
Timo <developer.timo@gmail.com>
2021-07-06 07:36:05 +0900

    숫자 포함 조건만 충족하는 경우 - 테스트에 통과하도록 코드 수정

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -11,6 +11,8 @@ public class PasswordStrengthMeter {
 
         if(lengthEnough && !containsNum && !containsUpp)
             return PasswordStrength.WEAK;
+        if(!lengthEnough && containsNum && !containsUpp)
+            return PasswordStrength.WEAK;
 
         if(!lengthEnough) return PasswordStrength.NORMAL;
         if(!containsNum) return PasswordStrength.NORMAL;
