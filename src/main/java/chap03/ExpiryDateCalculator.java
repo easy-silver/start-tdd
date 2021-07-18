0bfb870bb097fe1fa550379064291bfa292da6ae
Timo <developer.timo@gmail.com>
2021-07-19 00:37:50 +0900

    서비스 만료일 계산기 > 첫 납부일과 만료일 일자가 다를 때 2만원 이상 납부한 예외 상황 > 테스트 통과하도록 코드 수정

--- src/main/java/chap03/ExpiryDateCalculator.java
+++ src/main/java/chap03/ExpiryDateCalculator.java
@@ -1,6 +1,7 @@
 package chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
 
@@ -13,11 +14,17 @@ public class ExpiryDateCalculator {
         int addedMonths = payData.getPayAmount() / 10_000;
 
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+            LocalDate candidateExp =
+                    payData.getBillingDate().plusMonths(addedMonths);
 
             //첫 납부일과 납부일의 일자가 다르면 첫 납부일의 일자를 만료일의 일자로 사용
             if (payData.getFirstBillingDate().getDayOfMonth()
                     != candidateExp.getDayOfMonth()) {
+                if (YearMonth.from(candidateExp).lengthOfMonth() <
+                        payData.getFirstBillingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(
+                            YearMonth.from(candidateExp).lengthOfMonth());
+                }
                 return candidateExp.withDayOfMonth(
                         payData.getFirstBillingDate().getDayOfMonth());
             }
